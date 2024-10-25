9f8baf1fd82653691d592e2bb4cf733d6e355609
Jacquesval <33895664+Jacquesval@users.noreply.github.com>
2024-10-25 23:06:05 +0200

    Add files via upload

--- test_button.ino
+++ test_button.ino
@@ -11,12 +11,13 @@ int ledStates[4] = {0};
 long lastDebounceTime[4] = {0};
 int buttonsState[4] = {0};
 int lastButtonsState[4] = {0};
-
+int code[4] = {2, 2, 3, 1};
+int codePosition = 0;
 
 // variables will change:
-void readButton(int index){
+bool readButton(int index){
   int reading = digitalRead(buttons[index]);
-
+  bool buttonReleased = false;
   // check to see if you just pressed the button
   // (i.e. the input went from LOW to HIGH), and you've waited long enough
   // since the last press to ignore any noise:
@@ -34,16 +35,44 @@ void readButton(int index){
     // if the button state has changed:
     if (reading != buttonsState[index]) {
       buttonsState[index] = reading;
-
-      // only toggle the LED if the new button state is HIGH
-      if (buttonsState[index] == HIGH) {
-        ledStates[index] = !ledStates[index];
+      if(reading == LOW){
+        buttonReleased = true;
       }
     }
   }
 
   lastButtonsState[index] = reading;
+  return buttonReleased;
 }
+
+void setAllLedLeds(int state, int delayTime = 0){
+  for( int i= 0; i < 4; i++){
+    digitalWrite(leds[i], state);
+    delay(delayTime);
+  }
+}
+
+bool checkCode(int buttonIndex){
+  return (buttonIndex == (code[codePosition] - 1)); 
+}
+
+void blinkLeds(int iteration){
+  for(int i = 0; i < iteration; i++){
+    setAllLedLeds(LOW);
+    delay(100);
+    setAllLedLeds(HIGH);
+    delay(100);
+  }
+}
+
+void clearButtonState(){
+  for( int index = 0; index < 4; index++){
+    buttonsState[index] = 0;
+    lastButtonsState[index] = 0;
+  }
+}
+
+
 
 
 void setup() {
@@ -60,12 +89,23 @@ void setup() {
 
 int buttonState[4];
 int count = 0;
-void loop() {
-  readButton(count);
 
-  digitalWrite(leds[count], ledStates[count]);
+void loop() {
+  if(readButton(count)){
+    if(checkCode(count)){
+      digitalWrite(leds[codePosition], HIGH);
+      codePosition++;
+      if(codePosition == 4){
+        blinkLeds(4);
+        codePosition = 0;
+      }
+    }
+    else{
+      setAllLedLeds(0);
+      codePosition = 0;
+      clearButtonState();
+    }
+  }
 
   count = (count + 1) % 4;
-
-
 }
